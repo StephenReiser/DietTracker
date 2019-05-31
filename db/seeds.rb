6be70936076214ae35181f8a0f5a1370e09bfccb
StephenReiser <Stephen.Reiser1@gmail.com>
2019-05-31 13:33:20 -0400

    adding seed data to push to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create("email" => "test@gmail.com", "password" => "123", "password_confirmation" => "123")
+
+Meal.create("title" => "Lunch", "food_name" => ["Apple, Banana"], "sick" => "false", "comments" => "ate lots of stuff today", "user_id" => 1)
+
