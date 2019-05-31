ad9f914b8cfedec2ff39e855169a51b33235814a
StephenReiser <Stephen.Reiser1@gmail.com>
2019-05-31 13:38:14 -0400

    fixed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ User.create("email" => "test@gmail.com", "password" => "123", "password_confirma
 
 Meal.create("title" => "Lunch", "food_name" => ["Apple, Banana"], "sick" => "false", "comments" => "ate lots of stuff today", "user_id" => 1)
 
-Meal.create("title" => "Dinner", "food_name" => ["Chicken, Rice", "peppers"], "sick" => "true", "sick_type"=> "stomach, ""comments" => "Ugh!", "user_id" => 1)
+Meal.create("title" => "Dinner", "food_name" => ["Chicken, Rice", "peppers"], "sick" => "true", "sick_type"=> "stomach", "comments" => "Ugh!", "user_id" => 1)
 
 Meal.create("title" => "Breakfast", "food_name" => ["Apple, Banana"], "sick" => "false", "comments" => "MMMM Breakfast", "user_id" => 1)
 
