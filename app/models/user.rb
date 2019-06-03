1cac2e5f1fa3969e6756bafe46da53fc0ae36dd1
StephenReiser <Stephen.Reiser1@gmail.com>
2019-06-03 10:58:27 -0400

    tweaked backend to reject dup users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
     has_many :meals
 
     #Validations - might make sense to include
-#    validates_presence_of :name, :email, :password_digest
-#    validates :email, uniqueness: true
+   validates_presence_of :email, :password_digest
+   validates :email, uniqueness: true
 end
--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -1,3 +1,4 @@
+# require 'dotenv'
 Knock.setup do |config|
 
   ## Expiration claim
